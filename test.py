ce195c5cb7172ec27f91946a01882c64e1e38d41
高菲 <91829194+FayeGGG@users.noreply.github.com>
2022-12-27 21:58:17 +0800

    Update test.py

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import os
 from options.test_options import TestOptions
 from data import create_dataset
 from models import create_model
+from util.visualizer import save_images
+from util import html
 
 
 if __name__ == '__main__':
@@ -16,6 +18,13 @@ if __name__ == '__main__':
     model = create_model(opt)      # create a model given opt.model and other options
     model.setup(opt)               # regular setup: load and print networks; create schedulers
    
+    # create a website
+    web_dir = os.path.join(opt.results_dir, opt.name, '{}_{}'.format(opt.phase, opt.epoch))  # define the website directory
+    if opt.load_iter > 0:  # load_iter is 0 by default
+        web_dir = '{:s}_iter{:d}'.format(web_dir, opt.load_iter)
+    print('creating web directory', web_dir)
+    webpage = html.HTML(web_dir, 'Experiment = %s, Phase = %s, Epoch = %s' % (opt.name, opt.phase, opt.epoch))
+    
     if opt.eval:
         model.eval()
     for i, data in enumerate(dataset):
